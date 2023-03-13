cd1b5f14a8917c0e3efdd30444658b5cf89f7a65
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-13 02:00:59 +0200

    Update shuffle.py

--- YukkiMusic/plugins/admins/shuffle.py
+++ YukkiMusic/plugins/admins/shuffle.py
@@ -23,7 +23,7 @@ SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
 
 
 @app.on_message(
-    filters.command(["فنط","تفنيط","خلط","/shuffle],"")
+    filters.command(["فنط","تفنيط","خلط","/shuffle"],"")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
