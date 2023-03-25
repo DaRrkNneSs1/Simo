d1ddb914cff8870644f14dae6902f4185693f05b
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-25 16:43:12 +0200

    Update channel.py

--- YukkiMusic/plugins/play/channel.py
+++ YukkiMusic/plugins/play/channel.py
@@ -21,7 +21,7 @@ CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
 
 
 @app.on_message(
-    filters.command(CHANNELPLAY_COMMAND)
+    filters.command("قناه")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
