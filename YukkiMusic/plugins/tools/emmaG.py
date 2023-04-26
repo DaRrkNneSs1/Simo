f86f58b426cf763bf1451d028b88a937a4f7cfe3
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-04-26 19:52:33 +0200

    Update emmaG.py

--- YukkiMusic/plugins/tools/emmaG.py
+++ YukkiMusic/plugins/tools/emmaG.py
@@ -5,11 +5,6 @@ from pyrogram.types import Message
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from typing import Union
 from YukkiMusic import app
-
-@app.on_message(command("طرح"))
-async def Saidi(_, message: Message):
-    JABWA = "بلح"
-    await message.reply_text(AHMAD)
    
 @app.on_message(command("تويت"))
 async def bottttt(client, message):
